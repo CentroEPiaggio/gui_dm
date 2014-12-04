f5eec8d41cc74de0454fdeb1dae5a60f4355312a
Alessandro <ale.settimi@gmail.com>
2014-12-04 11:44:21 +0100

    camera_widget updated

--- include/widgets/camera_widget.h
+++ include/widgets/camera_widget.h
@@ -4,6 +4,9 @@
 #include <QWidget>
 #include <QGridLayout>
 #include <cstdlib>
+#include <rviz/visualization_manager.h>
+#include <rviz/render_panel.h>
+#include <rviz/display.h>
 
 class camera_widget: public QWidget
 {
@@ -15,6 +18,10 @@ public:
 private:
 
   QGridLayout main_layout;
+
+  rviz::RenderPanel render_panel;
+  rviz::VisualizationManager visualization_manager;
+  rviz::Display* camera_display;
 
 };
 
--- src/widgets/camera_widget.cpp
+++ src/widgets/camera_widget.cpp
@@ -1,8 +1,20 @@
 #include "widgets/camera_widget.h"
 
-camera_widget::camera_widget()
+camera_widget::camera_widget():visualization_manager(&render_panel)
 {
+    render_panel.setMinimumSize(30,30);
+    render_panel.initialize(visualization_manager.getSceneManager(),&visualization_manager);
 
+    visualization_manager.initialize();
+    visualization_manager.startUpdate();
+    visualization_manager.setFixedFrame("/base_link");
+
+    camera_display = visualization_manager.createDisplay("rviz/Camera", "camera", true );
+    camera_display->subProp("Image Topic")->setValue("/camera/rgb/image_raw");
+
+    main_layout.addWidget(camera_display->getAssociatedWidget(),0,0,Qt::AlignCenter);
+
+    setLayout(&main_layout);
 }
 
 camera_widget::~camera_widget()
