3454319b140780844b80b278f51a39837cc68db4
Alessandro <ale.settimi@gmail.com>
2015-03-02 12:45:03 +0100

    control widget updated

--- include/widgets/control_widget.h
+++ include/widgets/control_widget.h
@@ -9,6 +9,7 @@
 #include <map>
 #include "ros/ros.h"
 #include "dual_manipulation_shared/state_manager_service.h"
+#include "dual_manipulation_shared/ik_service.h"
 
 class control_widget: public QWidget
 {
@@ -19,16 +20,21 @@ public:
 
 private Q_SLOTS:
   void on_command_button_clicked(const int& id);
+  void on_stop_robot_button_clicked();
 
 private:
   QSignalMapper signalMapper;
   QGridLayout main_layout;
 
+  QPushButton stop_robot_button;
+
   std::map<int,QPushButton*> map_button;
 
   ros::NodeHandle n;
   ros::ServiceClient client;
+  ros::ServiceClient ik_client;
   dual_manipulation_shared::state_manager_service srv;
+  dual_manipulation_shared::ik_service ik_srv;
 };
 
 #endif // CONTROL_WIDGET_H
--- src/widgets/control_widget.cpp
+++ src/widgets/control_widget.cpp
@@ -1,8 +1,11 @@
 #include "widgets/control_widget.h"
 #include "ros/ros.h"
+#include "ros/package.h"
 
 control_widget::control_widget()
 {
+    QString path_to_package = QString::fromStdString(ros::package::getPath("dual_manipulation_gui"));
+
     int glob_id = 0;
     int row = 0;
     int col = 0;
@@ -14,6 +17,7 @@ control_widget::control_widget()
     command_vec.push_back("start_moving");
     command_vec.push_back("abort_move");
     command_vec.push_back("exit");
+    command_vec.push_back("reset");
 
     for(auto item:command_vec)
     {
@@ -25,19 +29,45 @@ control_widget::control_widget()
 	signalMapper.setMapping(button, glob_id) ;
 	glob_id++;
 	
-	if(glob_id == command_vec.size()/2+1)
+	if(glob_id == command_vec.size()/2+1 || glob_id == command_vec.size())
 	{
 	    row++;
 	    col=0;
+	    if(item=="reset") col=1;
 	}
 	main_layout.addWidget(button,row,col++,Qt::AlignCenter);
     }
     
     connect (&signalMapper, SIGNAL(mapped(int)), this, SLOT(on_command_button_clicked(int))) ;
     
+    stop_robot_button.setFixedSize(45,45);
+    stop_robot_button.setIcon(QIcon(path_to_package + "/stop.png"));
+    stop_robot_button.setIconSize( QSize(stop_robot_button.size().width(), stop_robot_button.size().height() ));
+
+    connect(&stop_robot_button,SIGNAL(clicked(bool)), this, SLOT(on_stop_robot_button_clicked()));
+
+    main_layout.addWidget(&stop_robot_button,row+2,1,Qt::AlignCenter);
+
     setLayout(&main_layout);
     
     client = n.serviceClient<dual_manipulation_shared::state_manager_service>("state_manager_ros_service");
+    ik_client = n.serviceClient<dual_manipulation_shared::ik_service>("ik_ros_service");
+}
+
+void control_widget::on_stop_robot_button_clicked()
+{
+    ROS_DEBUG_STREAM("command STOP to ik_control");
+
+    ik_srv.request.command = "stop";
+
+    if (ik_client.call(ik_srv))
+    {
+	ROS_INFO_STREAM("IK Control Request accepted: (" << (int)srv.response.ack << ")");
+    }
+    else
+    {
+	ROS_ERROR("Failed to call service dual_manipulation_shared::ik_service");
+    }
 }
 
 void control_widget::on_command_button_clicked(const int& id)
